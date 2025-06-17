3d5328c1719db6d47b09140033c14fb124084a3f
gilgyujeong <jgg7645@naver.com>
2025-06-17 05:41:18 +0000

    flask return fix

--- myapp/main.py
+++ myapp/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'hello world!'
+    return 'hello world! gilgyu!!!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8001)
